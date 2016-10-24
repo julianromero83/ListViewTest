e421af1e6165c587165d562a949b01287f2e029b
julianromero83 <rom16015@byui.edu>
2016-10-24 05:36:04 -0700

    Initial commit

---
+++ app/src/main/java/edu/byui/www/listviewtest/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.byui.www.listviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_main, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.myListView);
+        listView.setAdapter(adapter);
+
+    }
+}
